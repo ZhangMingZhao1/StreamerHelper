25aa9ec4159cd1bbebb0bf43434478ba38bdb0b2
umuoy1 <burningdian@gmail.com>
2020-09-13 02:27:10 +0800

    fix: fix time interval

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,7 @@ const timer = setInterval(() => {
     while (pool.length >= 1) {
         recorderPool.push(new Recorder(pool.pop()))
     }
-}, 3000);
+}, 120000);
 
 process.on("SIGINT", () => {
     console.log("Receive exit signal, clear the Interval, exit process.\nThe process exits when the upload task is complete.")
