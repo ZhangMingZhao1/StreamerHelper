b28972a9d94335c665dd88dfd85a75563acb73aa
umuoy1 <burningdian@gmail.com>
2021-04-06 17:22:27 +0800

    fix: file name

---
+++ src/type/recorderType.ts
@@ -0,0 +1,3 @@
+import { Recorder } from "@/engine/message";
+
+export type RecorderType = Required<Recorder>
---
+++ src/type/streamInfo.ts
@@ -0,0 +1,17 @@
+export interface StreamInfo {
+  timeV?: string;
+  roomLink: string;
+  roomName: string;
+  roomTags: string[];
+  streamUrl: string;
+  roomTid: number;
+  dirName?: string;
+  templateTitle?: string;
+  desc?: string;
+  source?: string;
+  dynamic?: string;
+  copyright?: number;
+  deleteLocalFile?: boolean;
+  uploadLocalFile?: boolean;
+  delayTime?: number
+}
