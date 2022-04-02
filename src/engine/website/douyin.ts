ff35998fb5c1a08bbf95b42ad98500bfb0b01342
倪小白 <mail@nixiaobai.com>
2022-04-02 23:36:06 +0800

    Update douyin.ts
    fix douyin

--- src/engine/website/douyin.ts
+++ src/engine/website/douyin.ts
@@ -1,43 +1,28 @@
-const axios = require("axios");
+const { chromium , devices} = require('playwright');
 
 export function main(url: string) {
-    return new Promise(function (resolve, reject) {
-        axios
-            .get(url)
-            .then(function (response: any) {
-                const html: string = response.data;
-                if (html.indexOf('直播已结束')!= -1){
-                    reject(
-                        "DOUYIN=>No match results:Maybe the roomid is error,or this room is not open!"
-                    );
-                }
-                const rid: any = html.match(
-                    /(?<="room_id_str":")(.+?)(?=",")/g
-                );
-                axios
-                    .get(
-                        `https://webcast-hl.amemv.com/webcast/room/reflow/info/?room_id=${rid[0]}&live_id=1`
-                    )
-                    .then(function (response: any) {
-                        const json: any = response.data;
-                        if (json && json["status_code"] == 0) {
-                            resolve(
-                                json["data"]["room"]["stream_url"][
-                                    "rtmp_pull_url"
-                                ]
-                            );
-                        } else {
-                            reject(
-                                "DOUYIN=>No match results:Maybe the roomid is error,or this room is not open!"
-                            );
-                        }
-                    })
-                    .catch(function (error: any) {
-                        reject(error);
-                    });
-            })
-            .catch(function (error: any) {
+    return new Promise(async function (resolve, reject) {
+
+            const browser:any = await chromium.launch({ headless: true })
+            
+            const phoneD:any = devices['Desktop Edge']
+            const context:any = await browser.newContext({ ...phoneD })
+        
+            const page:any = await context.newPage();
+            
+            await page.goto(url)
+        
+            const html = page.innerHTML("body");
+            const flvurl:any = await  html.then(function(result:any){
+                browser.close()
+                const flv = result.match(/(http.*.flv)/g);
+                resolve(flv[0]);
+            }).catch(function(error:any){
                 reject(error);
+                browser.close();
             });
+            
+            resolve(flvurl);
+
     });
 }
